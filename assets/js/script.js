618b49c5ab04b2345e0df10c1fe7c908c144d1c9
roo <roo116@nc.rr.com>
2022-05-18 16:16:28 -0400

    started on issue #2.  updated html to explore how to style the time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,14 @@
 // Add custom color styles to an existing CSS framework
 // Work with custom fonts and icons via a content delivery network (CDN)
 
-// WHEN I open the planner THEN the current day is displayed at the top of the calendar
-// how do I create the elements for the calendar.  how did taskmaster pro do it?
-//moment() can show the current day. 
 
-//creating current date in jumbo-tron
+//shows current day at the top of the calendar
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-
-
-
-
-//I may need code to check out how to make sure time is dealt with (again moment)
-
-
-
-
 // WHEN I scroll down THEN I am presented with time blocks for standard business hours
+// show timeblocks for standard business hours
+
+
 
 
 
