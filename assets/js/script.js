4a67939bf495491ef6495be98358ebc45cd959e5
roo <roo116@nc.rr.com>
2022-05-18 14:59:58 -0400

    centered jumbo-tron text

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// Implement third-party libraries such as Bootstrap, jQuery, and Moment.js
+// DOM traversals and manipulations with jQuery
+// jQuery to save user input in local storage
+// Bootstrap grid layout in HTML.  Bootstrap components
+// Add custom color styles to an existing CSS framework
+// Work with custom fonts and icons via a content delivery network (CDN)
+
+// WHEN I open the planner THEN the current day is displayed at the top of the calendar
+// how do I create the elements for the calendar.  how did taskmaster pro do it?
+//moment() can show the current day. 
+
+//I may need code to check out how to make sure time is dealt with (again moment)
+
+
+
+
+// WHEN I scroll down THEN I am presented with time blocks for standard business hours
+
+
+
+
+
+// WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+
+
+
+
+// WHEN I click into a time block THEN I can enter an event
+
+
+
+
+
+// WHEN I click the save button for that time block THEN the text for that event is saved in local storage
+
+
+
+
+
+// WHEN I refresh the page THEN the saved events persist
